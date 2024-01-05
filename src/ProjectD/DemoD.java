a11d8c71f14a5ee4805f3c4a30ca06aec55aa2c9
vflei <vflei@Vladimir.attlocal.net>
2024-01-04 20:19:46 -0500

    Initial Commit

---
+++ src/ProjectD/DemoD.java
@@ -0,0 +1,14 @@
+package ProjectD;
+
+public class DemoD {
+	
+public static void main(String[]args) {
+		
+		System.out.println("Monday");
+		
+		System.out.println("Git project push...");
+		System.out.println("Git project pull...");
+		
+	}
+
+}
